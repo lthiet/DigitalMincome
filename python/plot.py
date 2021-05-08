b42e029cdd9474b196f9bd44625ab4edc83f6c3a
TheSpecialOne45 <francois.p.45000@gmail.com>
2021-05-08 15:46:22 +0200

    plot prout

--- python/plot.py
+++ python/plot.py
@@ -1,10 +1,8 @@
-import plotly.express as px
+""" import plotly.express as px
 import os
 
 cwd = os.getcwd()
 plot_html_path = os.path.join(os.path.dirname(cwd),'DigitalMincome/app/plot.html')
-
-
 
 df = px.data.gapminder()
 fig = px.scatter(df, x="gdpPercap", y="lifeExp", animation_frame="year", animation_group="country",
@@ -12,4 +10,58 @@ fig = px.scatter(df, x="gdpPercap", y="lifeExp", animation_frame="year", animati
            log_x=True, size_max=55, range_x=[100,100000], range_y=[25,90])
 
 fig["layout"].pop("updatemenus") # optional, drop animation buttons
-fig.write_html(plot_html_path)
+fig.write_html(plot_html_path) """
+
+import plotly.graph_objects as go
+import numpy as np
+import os
+
+# Create figure
+fig = go.Figure()
+
+# Add traces, one for each slider step
+for step in np.arange(0, 5, 0.1):
+    fig.add_trace(
+        go.Scatter(
+            visible=False,
+            line=dict(color="#00CED1", width=6),
+            name="𝜈 = " + str(step),
+            x=np.arange(0, 10, 0.01),
+            y=np.sin(step * np.arange(0, 10, 0.01))))
+
+# Make 10th trace visible
+fig.data[10].visible = True
+
+# Create and add slider
+steps = []
+for i in range(len(fig.data)):
+    step = dict(
+        method="update",
+        args=[{"visible": [False] * len(fig.data)},
+              {"title": "Slider switched to step: " + str(i)}],  # layout attribute
+    )
+    step["args"][0]["visible"][i] = True  # Toggle i'th trace to "visible"
+    steps.append(step)
+
+sliders = [dict(
+    active=10,
+    currentvalue={"prefix": "Frequency: "},
+    pad={"t": 50},
+    steps=steps
+)]
+
+fig.update_layout(
+    sliders=sliders
+)
+
+
+
+cwd = os.getcwd()
+plot_html_path = os.path.join(os.path.dirname(cwd),'DigitalMincome/app/plot.html')
+
+fig.write_html(plot_html_path) 
+
+
+
+
+
